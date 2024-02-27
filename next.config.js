918a74aba9c1574856dd0fee9a89a7a351df4b73
bard <bartoszslesar@gmail.com>
2024-02-27 20:32:49 +0100

    finished page of next.js job page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "xynovwsavc1tp5oz.public.blob.vercel-storage.com",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/lib/job-types.ts
@@ -0,0 +1,10 @@
+export const jobTypes = [
+  "Full-time",
+  "Part-time",
+  "Concract",
+  "Temporary",
+  "Internship",
+  "Volunteer",
+];
+
+export const locationTypes = ["Remote", "On-site", "Hybrid"];
